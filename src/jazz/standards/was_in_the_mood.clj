4c5a482578d0cba31e31fa5180f011cd682a75d0
Chris Ford <christophertford@gmail.com>
2018-12-04 04:57:14 +0100

    Ready for performance.

--- src/jazz/standards/was_in_the_mood.clj
+++ src/jazz/standards/was_in_the_mood.clj
@@ -1,4 +1,4 @@
-(ns jazz.standards.in-the-mood
+(ns jazz.standards.was-in-the-mood
   (:use leipzig.scale, leipzig.melody, leipzig.live, leipzig.chord, leipzig.temperament
         [overtone.live :only [play-buf load-sample freesound-path sample apply-at apply-by now at ctl square perc FREE adsr line:kr sin-osc definst hpf lpf clip2 env-gen]]
         jazz.instruments))
@@ -8,17 +8,25 @@
 (def fifth (-> triad (root 4)))
 
 (def in-the-mood
-  (let [bassline (->> [0]
-                      (phrase [1]))
-        hook #(->> (-> % list)
-                   (phrase [8]))
+  (let [bassline #(->> [0 2 4 5 4 7 5 4 2]
+                       (phrase [1 1 1 1/2 1/2 1 1 1 1])
+                       (where :pitch (comp lower lower))
+                       (where :pitch (from %)))
+        hook (mapthen #(phrase (concat (repeat 11 1/2) [5/2])
+                               (-> % vals sort cycle)
+                               ;(repeat %)
+                               )
+                      [tonic tonic fourth tonic fifth tonic])
         beat (->> (rhythm (cycle [1 1/2 1/2]))
                   (take-while #(-> % :time (< 48)))
                   (all :part :beat))]
     (->>
-      bassline
+      (mapthen bassline [0 0 3 0 4 0])
+      (with hook)
+      (with beat)
+      (tempo (comp (scale [2/3 1/3]) (partial * 2)))
       (where :pitch (comp low B flat major))
-      (tempo (bpm 140)))))
+      (tempo (bpm 120)))))
 
 (comment
   (jam (var in-the-mood))
