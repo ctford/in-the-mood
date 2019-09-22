725e7f86f4d5291329776bcaf2e7edac752c3402
Chris Ford <christophertford@gmail.com>
2019-09-22 17:43:10 +0200

    Chords are for cowards.

--- src/jazz/standards/in_the_mood.clj
+++ src/jazz/standards/in_the_mood.clj
@@ -1,6 +1,2 @@
 (ns jazz.standards.in-the-mood
   (:use leipzig.scale, leipzig.melody, leipzig.chord, leipzig.live jazz.instruments))
-
-(def tonic (-> triad (root 7) (inversion 1)))
-(def fourth (-> triad (root 3)))
-(def fifth (-> triad (root 4)))
