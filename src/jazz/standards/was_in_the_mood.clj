a5dcdbbe11778266fd3e8cfb148976dc5c2ac92c
Chris Ford <christophertford@gmail.com>
2019-09-22 11:49:38 +0200

    Clean up pre-prepared version.

--- src/jazz/standards/was_in_the_mood.clj
+++ src/jazz/standards/was_in_the_mood.clj
@@ -1,7 +1,6 @@
 (ns jazz.standards.was-in-the-mood
   (:use leipzig.scale, leipzig.melody, leipzig.live, leipzig.chord, leipzig.temperament
-        [overtone.live :only [now play-buf load-sample freesound-path sample apply-at apply-by now at ctl square perc FREE adsr line:kr sin-osc definst hpf lpf clip2 env-gen]]
-        jazz.instruments
+        [overtone.live :only [now]]
         [quil.core :only
          [color clear smooth sketch ellipse frame-rate background
           width height stroke stroke-weight fill]]))
@@ -10,37 +9,34 @@
 (def fourth (-> triad (root 3)))
 (def fifth (-> triad (root 4)))
 
-(def bar 8)
+(def bar 7)
 
 (def in-the-mood
-  (let [bassline #(->> [0 2 4 5 4 7 5 4 2]
-                       (phrase [1 1 1 1/2 1/2 1 1 1 1])
-                       (cut bar)
-                       (where :pitch (comp lower lower))
-                       (where :pitch (from %)))
-        hook (mapthen #(->> (phrase (repeat 12 1/2)
-                            ;        (->> % vals sort cycle)
-                            (repeat %)
-                                    )
-                            (then (phrase (repeat 5 1/2) (repeat nil)))
-                            (cut bar)
-                            )
-                      [tonic tonic fourth tonic fifth tonic])
+  (let [bassline (fn [root]
+                   (->> [0 2 4 5 4 7 5 4 2]
+                        (phrase [1 1 1 1/2 1/2 1 1 1 1])
+                        (cut bar)
+                        (where :pitch (comp lower lower))
+                        (where :pitch (from root))))
+        hook (fn [chord]
+               (->> chord
+                    vals sort cycle
+                    ;repeat
+                    (phrase (concat (repeat 11 1/2) [5/2]))
+                    (cut bar)))
         beat (->> (rhythm (cycle [1 1/2 1/2]))
-                  (cut (* bar 6))
+                  (cut bar)
                   (all :part :beat))]
     (->>
       (mapthen bassline [0 0 3 0 4 0])
-      (with hook)
-      (with beat)
+      (with (mapthen hook [tonic tonic fourth tonic fifth tonic]))
+      (with (times 6 beat))
       (tempo (comp (scale [2/3 1/3]) (partial * 2)))
-      (where :pitch (comp D major))
+      (where :pitch (comp C major))
       (tempo (bpm 120)))))
 
 (comment
   (jam (var in-the-mood) play-at)
-  (play play-at in-the-mood)
-  (play-at (now) (second in-the-mood))
   (def in-the-mood nil)
 )
 
@@ -64,7 +60,7 @@
                (doseq [epoch (take-last 1 hits-played)]
                  (ellipse
                    (mod (/ epoch 30) 923)
-                    (+ 100 (/ (- current epoch) 20)) 
+                    (+ 100 (/ (- current epoch) 20))
                    50
                    50))
                (fill (color 234 123 28))
@@ -74,16 +70,13 @@
                    (+ 50 (/ (- current (:time note)) 20))
                    10
                    (* 100 (:duration note))))))
-    :size [1024 768])
-  )
+    :size [1024 768]))
 
 (defmethod play-note :beat [{epoch :time}]
-  (do
-    (swap! hits conj epoch)
-    (hat)))
+  (swap! hits conj epoch)
+  (hat))
 
 (defmethod play-note :default
   [{midi :pitch, seconds :duration :as note}]
-  (do
-    (swap! plinks conj note)
-    (-> midi (piano :duration seconds))))
+  (swap! plinks conj note)
+  (-> midi (piano :duration seconds)))
